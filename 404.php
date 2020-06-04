5fdbdeb42b7b8513130024e8c292ae70c54c7297
lexvitorino <miguel032014>
2020-06-03 21:47:16 -0300

    página 404

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<section>
+    <div class="container">
+        <h1>Página não encontrada</h1>
+    </div>
+    <div style="clear:both"></div>
+</section>
+
+<?php get_footer(); ?>
